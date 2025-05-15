91c11dc5130dc6d0a09468ca05821cd7840e617b
Screxy <dvbvladis@mail.ru>
2025-05-15 19:41:00 +0300

    feat: signup method

---
+++ users/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from users.models import CustomUser
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    
+    class Meta:
+        model = CustomUser
+        fields = ('username', 'email', 'password', 'first_name', 'last_name')
+    
+    def create(self, validated_data):
+        user = CustomUser.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password'],
+            first_name=validated_data.get('first_name', ''),
+            last_name=validated_data.get('last_name', '')
+        )
+        return user
